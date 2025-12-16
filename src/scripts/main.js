7d228b261cb0c5948e28aa5698375e6941e8477f
Andrey <kagamlykas@gmail.com>
2025-12-17 01:13:58 +0200

    Solved task by feedback 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,17 +3,19 @@
 document.addEventListener('click', (e) => {
   const wall = document.querySelector('.wall');
   const spider = document.querySelector('.spider');
+  const wallRect = wall.getBoundingClientRect();
+
 
   if (wall && spider) {
-    const borderStartX = wall.offsetLeft + wall.clientLeft;
-    const borderStartY = wall.offsetTop + wall.clientTop;
+    const borderStartX = wallRect.left + wall.clientLeft;
+    const borderStartY = wallRect.top + wall.clientTop;
 
     const borderEndX = borderStartX + wall.clientWidth;
     const borderEndY = borderStartY + wall.clientHeight;
 
     const midSpider = spider.clientHeight / 2;
-    const currentX = e.x;
-    const currentY = e.y;
+    const currentX = e.clientX;
+    const currentY = e.clientY;
 
     if (
       currentX >= borderStartX &&
