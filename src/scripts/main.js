8e3e18c0596ea0338ac2ca5ff86f16970d114395
Andrey <kagamlykas@gmail.com>
2025-12-17 00:59:23 +0200

    Solved task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,50 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  if (wall && spider) {
+    const borderStartX = wall.offsetLeft + wall.clientLeft;
+    const borderStartY = wall.offsetTop + wall.clientTop;
+
+    const borderEndX = borderStartX + wall.clientWidth;
+    const borderEndY = borderStartY + wall.clientHeight;
+
+    const midSpider = spider.clientHeight / 2;
+
+    wall.addEventListener('mousemove', (ev) => {
+      const currentX = ev.x;
+      const currentY = ev.y;
+
+      if (
+        currentX >= borderStartX &&
+        currentX <= borderEndX &&
+        currentY >= borderStartY &&
+        currentY <= borderEndY
+      ) {
+        let setX = currentX - borderStartX - midSpider;
+        let setY = currentY - borderStartY - midSpider;
+
+        if (currentX >= borderEndX - midSpider) {
+          setX = wall.clientWidth - spider.clientWidth;
+        }
+
+        if (currentX <= borderStartX + midSpider) {
+          setX = 0;
+        }
+
+        if (currentY >= borderEndY - midSpider) {
+          setY = wall.clientHeight - spider.clientHeight;
+        }
+
+        if (currentY <= borderStartY + midSpider) {
+          setY = 0;
+        }
+
+        spider.style.top = `${setY}px`;
+        spider.style.left = `${setX}px`;
+      }
+    });
+  }
 });
